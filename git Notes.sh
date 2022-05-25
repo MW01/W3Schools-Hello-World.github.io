daffb266feb3b6d2308dd42ba7bed64b3e60f3b2
MW <MikeWebster01@Gmail.com>
2022-05-25 17:31:04 -0400

    more notes..

--- git Notes.sh
+++ git Notes.sh
@@ -1,19 +1,24 @@
+# Version and Configuration
 git --version
 git config --global user.name "w3schools-test"
 git config --global user.email "test@w3schools.com"
+
+# Starting a new project
 mkdir myproject
 cd myproject
 git init 
 git status
 git add filename.extension
+    # git -a # will stage every already tracked file that has been modified
 <<'NOTE'
     # or  git add --all
     #...The shorthand for git add --all is git add -A
     #(use "git rm --cached <file>..." to unstage)
 NOTE
 
+# Commit 
 git commit -m "First release of Hello World!"
-git -a # will stage every already tracked file that has been modified
+    # git -a # will stage every already tracked file that has been modified
 
 git status --short
 <<'NOTE.'
@@ -24,5 +29,5 @@ git status --short
 #     D - Deleted files
 NOTE.
 
-
+# 
 
