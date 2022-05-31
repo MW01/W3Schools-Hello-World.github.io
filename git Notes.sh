f7b71d16f731dee67aa8a7cfdeeb1b372f737027
MW <MikeWebster01@Gmail.com>
2022-05-31 14:37:08 -0400

    Day 2

--- git Notes.sh
+++ git Notes.sh
@@ -8,26 +8,35 @@ mkdir myproject
 cd myproject
 git init 
 git status
+
+# Staging files 
+#   - whenever you hit a milestone or finish a part of the work, you should add the files to a Staging Environment
+#   - Staged files are files that are ready to be committed to the repository you are working on
 git add filename.extension
-    # git -a # will stage every already tracked file that has been modified
+git add --all # Using --all instead of individual filenames will stage all changes (new, modified, and deleted) files
+              # Note: The shorthand command for git add --all is git add -A
+#To commit without staging # git -a # will stage every already tracked file that has been modified
 <<'NOTE'
     # or  git add --all
     #...The shorthand for git add --all is git add -A
     #(use "git rm --cached <file>..." to unstage)
 NOTE
 
-# Commit 
+# Committing Changes
 git commit -m "First release of Hello World!"
     # git -a # will stage every already tracked file that has been modified
 
 git status --short
 <<'NOTE.'
-# Note: Short status flags are:
-#     ?? - Untracked files
-#     A - Files added to stage
-#     M - Modified files
-#     D - Deleted files
+    # Note: Short status flags are:
+    #     ?? - Untracked files
+    #     A - Files added to stage
+    #     M - Modified files
+    #     D - Deleted files
 NOTE.
 
-# 
+# Merging a branch
+git merge branch-name     # to merge 
 
+# Deleting a bracnch
+git branch -d branch-name # to delete the branch 
